20c8e45ea2e4c769703cb8d3d5a73cf061b7ca73
Rufae <rafaelcastanoblanca1805@gmail.com>
2025-08-09 12:04:33 +0200

    Arreglos de export

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -5,7 +5,7 @@ import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
 import { type VariantProps } from "class-variance-authority";
 
 import { cn } from "./utils";
-import { toggleVariants } from "./toggle";
+import { toggleVariants } from "./toggleVariants";
 
 const ToggleGroupContext = React.createContext<
   VariantProps<typeof toggleVariants>
